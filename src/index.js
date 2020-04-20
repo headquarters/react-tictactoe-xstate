7d70e8b1d39122d3fe95c682f82617cbc0374bfa
Michael Head <michael.head@slalom.com>
2020-04-20 16:28:28 -0400

    xstate replaces all previous state management

--- src/index.js
+++ src/index.js
@@ -1,158 +1,295 @@
-import './index.css';
+import "./index.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { Machine, actions, assign, interpret } from "xstate";
 
-function Square(props) {
-    return (
-      <button className="square" onClick={props.onClick}>
-        {props.value}
-      </button>
-    );
-  }
-  
-  class Board extends React.Component {
-    renderSquare(i) {
-      return (
-        <Square
-          value={this.props.squares[i]}
-          onClick={() => this.props.onClick(i)}
-        />
-      );
-    }
-  
-    render() {
-      return (
-        <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        history: [
+import React from "react";
+import ReactDOM from "react-dom";
+
+const { log } = actions;
+
+const play = (squares, position, value) => {
+  const localSquares = [...squares];
+  localSquares[position] = value;
+  return localSquares;  
+}
+
+const ticTacToeMachine = Machine({
+  id: "tictactoe",
+  initial: "start",
+  context: {
+    squares: Array(9).fill(null),
+    status: "Next player: X",
+    winner: null
+  },
+  states: {
+    start: {
+      on: {
+        // X plays first
+        PLAY: {
+          actions: assign({
+            status: "Next player: O",
+            squares: (ctx, event) => play(ctx.squares, event.value, "X")
+          }),
+          target: "oTurn"
+        }
+      }
+    },
+    xTurn: {
+      on: {
+        "": [
           {
-            squares: Array(9).fill(null)
+            target: "gameOver",
+            cond: "gameOverGuard"
           }
         ],
-        stepNumber: 0,
-        xIsNext: true
-      };
-    }
-  
-    handleClick(i) {
-      const history = this.state.history.slice(0, this.state.stepNumber + 1);
-      const current = history[history.length - 1];
-      const squares = current.squares.slice();
-      if (calculateWinner(squares) || squares[i]) {
-        return;
+        PLAY: [{
+          actions: assign({
+            status: "Next player: O",
+            squares: (ctx, event) => play(ctx.squares, event.value, "X")
+          }),
+          target: "oTurn",
+          // Do not allow transitioning when a square is already occupied 
+          cond: (ctx, event) => (ctx.squares[event.value]) === null
+        },
+        ],
       }
-      squares[i] = this.state.xIsNext ? "X" : "O";
-      this.setState({
-        history: history.concat([
+    },
+    oTurn: {
+      on: {
+        "": [
           {
-            squares: squares
+            target: "gameOver",
+            cond: "gameOverGuard"
           }
-        ]),
-        stepNumber: history.length,
-        xIsNext: !this.state.xIsNext
-      });
-    }
-  
-    jumpTo(step) {
-      this.setState({
-        stepNumber: step,
-        xIsNext: (step % 2) === 0
-      });
-    }
-  
-    render() {
-      const history = this.state.history;
-      const current = history[this.state.stepNumber];
-      const winner = calculateWinner(current.squares);
-  
-      const moves = history.map((step, move) => {
-        const desc = move ?
-          'Go to move #' + move :
-          'Go to game start';
-        return (
-          <li key={move}>
-            <button onClick={() => this.jumpTo(move)}>{desc}</button>
-          </li>
-        );
-      });
-  
-      let status;
-      if (winner) {
-        status = "Winner: " + winner;
-      } else {
-        status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+        ],
+        PLAY: [
+          {
+            actions: assign({
+              status: "Next player: X",
+              squares: (ctx, event) => play(ctx.squares, event.value, "O")
+            }),
+            target: "xTurn",
+            // Do not allow transitioning when a square is already occupied
+            cond: (ctx, event) => (ctx.squares[event.value]) === null
+          },
+        ],
       }
-  
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board
-              squares={current.squares}
-              onClick={i => this.handleClick(i)}
-            />
-          </div>
-          <div className="game-info">
-            <div>{status}</div>
-            <ol>{moves}</ol>
-          </div>
-        </div>
-      );
+    },
+    gameOver: {
+      entry: log("Game Over!"),
+      on: {
+        "": [
+          {
+            target: "xWinner",
+            cond: "xWins",
+            actions: assign({
+              status: "Winner: X",
+              winner: "X"
+            })
+          },
+          {
+            target: "oWinner",
+            cond: "oWins",
+            actions: assign({
+              status: "Winner: O",
+              winner: "O"
+            })
+          },
+          {
+            target: "noWinner",
+            cond: "noOneWins",
+            actions: assign({
+              status: "Winner: 😸",
+              winner: "😸"
+            })
+          }
+        ]
+      }
+    },
+    xWinner: {
+      entry: log("X wins!"),
+      on: {
+        STARTOVER: {
+          actions: assign({
+            squares: Array(9).fill(null),
+            status: "Next player: X",
+            winner: null
+          }),
+          target: "start"
+        }
+      }
+    },
+    oWinner: {
+      entry: log("O wins!"),
+      on: {
+        STARTOVER: {
+          actions: assign({
+            squares: Array(9).fill(null),
+            status: "Next player: X",
+            winner: null
+          }),
+          target: "start"
+        }
+      }
+    },
+    noWinner: {
+      entry: log("No one won"),
+      on: {
+        STARTOVER: {
+          actions: assign({
+            squares: Array(9).fill(null),
+            status: "Next player: X",
+            winner: null
+          }),
+          target: "start"
+        }
+      }
     }
   }
-  
-  // ========================================
-  
-  ReactDOM.render(<Game />, document.getElementById("root"));
-  
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
+},
+  {
+    guards: {
+      gameOverGuard: (context, event) => {
+        const winner = calculateWinner(context.squares);
+        return winner || isGameOver(context, event);
+      },
+      xWins: (context, event) => {
+        const winner = calculateWinner(context.squares);
+        return winner === "X";
+      },
+      oWins: (context, event) => {
+        const winner = calculateWinner(context.squares);
+        return winner === "O";
+      },
+      noOneWins: (context, event) => {
+        const winner = calculateWinner(context.squares);
+        return winner === null;
+      },
     }
-    return null;
   }
-  
-  
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
+);
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
   );
-  
+}
+
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  state = {
+    current: ticTacToeMachine.initialState
+  };
+
+  service = interpret(ticTacToeMachine).onTransition(current => {
+    console.log({ current })
+    this.setState({ current });
+  });
+
+  componentDidMount() {
+    this.service.start();
+  }
+
+  componentWillUnmount() {
+    this.service.stop();
+  }
+
+  handleClick(value) {
+    const { send } = this.service;
+    send("PLAY", { value });
+  }
+
+  startOver() {
+    const { send } = this.service;
+    send("STARTOVER");
+  }
+
+  render() {
+    const { current } = this.state;
+
+    const squares = current.context.squares;
+    const status = current.context.status;
+    const allowStartOver = !current.nextEvents.includes("STARTOVER");
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board squares={squares}
+            onClick={(value) => this.handleClick(value)} />
+        </div>
+        <div className="game-info">
+          <div className="status">{status}</div>
+          <button disabled={allowStartOver} onClick={() => this.startOver()}>New Game</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+function isGameOver(context, event) {
+  const squares = context.squares;
+  const squaresFull = squares.every(value => value !== null);
+
+  return squaresFull;
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById("root")
+);
